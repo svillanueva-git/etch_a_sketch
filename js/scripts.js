8f559bce8e5819c05f963f18c6d690c778958abf
Sincere Villanueva III <svillanueva.git@gmail.com>
2021-01-09 13:47:34 -0800

    developed a basic hover effect

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 const container = document.querySelector('#container');
 container.classList.add('addGrid');
 
+
 for (let i = 0; i < 256; i++) {
     const divs = document.createElement('div');
+    divs.classList.add('squares');
     container.appendChild(divs);
 }
 
-
-
+const divDiv = document.querySelectorAll('div div');
+container.addEventListener('mouseover', function(e) {
+    e.target.style.background = 'black';
+})
